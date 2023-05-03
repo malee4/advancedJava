1eb481d1d447c4820622cb249a47968740375b89
Melody Lee <melody.lee357@gmail.com>
2023-05-03 18:51:43 -0400

    adding scroll bar and color for background

--- Unit5/Timer/TimerApp.java
+++ Unit5/Timer/TimerApp.java
@@ -13,23 +13,25 @@ import javafx.scene.paint.Color;
 
 public class TimerApp extends Application{
     private Color color = Color.WHITE;
-    // private Background background = new Background(new BackgroundFill(color));
+    public static Background background = new Background(new BackgroundFill(Color.RED, null, null));
+    public static VBox container = new VBox(20);
     
-
     @Override
     public void start(Stage primaryStage) {
-        VBox container = new VBox(20);
+        
         Insets padding = new Insets(30); // aesthetics
         container.setPadding(padding);
 
         // create the timer
-        VBox timer = new TimerComponent(30).getContainer();
+        VBox timer = new TimerComponent(5).getContainer();
         timer.setAlignment(Pos.CENTER);
 
         // collect and display the tasks
         VBox taskList = new Tasks().getContainer();
 
         container.getChildren().addAll(timer, taskList);
+
+        container.setBackground(background);
 
         Scene scn = new Scene(container, 400, 400);
         primaryStage.setTitle("Focus Timer");
--- Unit5/Timer/TimerComponent.java
+++ Unit5/Timer/TimerComponent.java
@@ -1,6 +1,9 @@
 package Unit5.Timer;
 
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import java.util.Timer;
@@ -46,9 +49,13 @@ public class TimerComponent {
           if (paused) {
             startPauseButton.setText("Pause");
             paused = false;
-
+            TimerApp.background = new Background(new BackgroundFill(Color.GREEN, null, null));
+            TimerApp.container.setBackground(TimerApp.background);
+            
             timerTask = new PomodoroTimerTask(length, timeElapsed -> timeElapsedLabel.setText(secondsToTimeString(length - timeElapsed)), () -> {
               paused = true;
+              TimerApp.background = new Background(new BackgroundFill(Color.RED, null, null));
+              TimerApp.container.setBackground(TimerApp.background);
               startPauseButton.setText("Start");
             });
             // timerTask = new PomodoroTimerTask(length, timeElapsed -> System.out.println("hii " + timeElapsed), () -> {});
